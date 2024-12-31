2cf282367b790ca866b448b3736038fb287e64e4
Joshua Taylor Eppinette <josheppinette@gmail.com>
2024-12-31 14:16:09 -0700

    Fix syntax error in vimf alias

--- home.nix
+++ home.nix
@@ -76,7 +76,7 @@
   programs.zsh = {
     enable = true;
     shellAliases = {
-      vimf = ''nvim - o "$(fzf)"'';
+      vimf = ''nvim -o "$(fzf)"'';
       fda = "fd --hidden --exclude .git --exclude .direnv --exclude __pycache__ --type f";
       du = "du -A";
     };
